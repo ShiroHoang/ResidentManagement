0ee9a07c2b4f2116feca2a1ae38f4d84b027721a
huynq <huynguyensteph@gmail.com>
2025-02-25 20:59:28 +0700

    feat(AddressRegistation): update form for registration

--- ResidentManagement/src/java/dal/AddressRegistryDAO.java
+++ ResidentManagement/src/java/dal/AddressRegistryDAO.java
@@ -7,6 +7,8 @@ package dal;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import model.*;
 
@@ -15,19 +17,20 @@ import model.*;
  * @author huyng
  */
 public class AddressRegistryDAO extends DBContext {
-        public User getAccount(String user, String pass) {
-        String sql = "select * from Users where "
-                + "Email =? and Password = ?";
+
+    public List<AddressRegistry> getAll() {
+        String sql = "select * from AddressRegistry";
         try {
             PreparedStatement stmt = connection.prepareStatement(sql);
-            stmt.setString(1, user);
-            stmt.setString(2, pass);
             ResultSet rs = stmt.executeQuery();
+            List<AddressRegistry> list = new ArrayList<>();
             while (rs.next()) {
-                return new User(rs.getInt("UserId"), rs.getString("FullName"),
-                        rs.getString("Email"), rs.getString("Password"), rs.getString("Role"), rs.getString("Address"), rs.getString("PhoneNumber")
-                );
+                list.add(new AddressRegistry(rs.getInt("AddressID"), rs.getString("Province"),
+                        rs.getString("City"), rs.getString("District"), rs.getString("Ward"), 
+                        rs.getString("Street"), rs.getString("HouseNumber")));
+                ;
             }
+            return list;
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
