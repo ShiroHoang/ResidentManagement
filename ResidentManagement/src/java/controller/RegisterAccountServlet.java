68afbe8dfeae54d5c3fb5f414b0792446e9da3b3
ShiroHoang <136798761+ShiroHoang@users.noreply.github.com>
2025-02-27 13:19:19 +0700

    fix (servlet registerAccount): fix href

--- ResidentManagement/src/java/controller/RegisterAccountServlet.java
+++ ResidentManagement/src/java/controller/RegisterAccountServlet.java
@@ -55,7 +55,7 @@ public class RegisterAccountServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        processRequest(request, response);
+        request.getRequestDispatcher("view/registerAccount.jsp").forward(request, response);
     } 
 
     /** 
