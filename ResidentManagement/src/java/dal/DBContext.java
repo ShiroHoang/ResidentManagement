7ed198116404bbbfd63b25a20902cefac09e44e6
huynq <huynguyensteph@gmail.com>
2025-02-27 22:51:08 +0700

    refactor (DBContext): hide SQL secrets

--- ResidentManagement/src/java/dal/DBContext.java
+++ ResidentManagement/src/java/dal/DBContext.java
@@ -1,8 +1,11 @@
 package dal;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,15 +13,15 @@ import java.util.logging.Logger;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this licenseaccount
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author FPT University - PRJ30X
  */
 public class DBContext {
+
     protected Connection connection;
-    public DBContext()
-    {
+
+    public DBContext() {
         //@Students: You are allowed to edit user, pass, url variables to fit 
         //your system configuration
         //You can also add more methods for Database Interaction tasks. 
@@ -26,12 +29,22 @@ public class DBContext {
         // For example : StudentDBContext extends DBContext , 
         //where StudentDBContext is located in dal package, 
         try {
-            String user = "sa";
-            String pass = "sa123@@@";
-            String url = "jdbc:sqlserver://47.129.203.47:1433;databaseName=ResidentManagement";
+            Properties props = new Properties();
+
+            // Load db.properties from classpath
+            InputStream input = DBContext.class.getClassLoader().getResourceAsStream("db.properties");
+            if (input == null) {
+                throw new IOException("db.properties file not found in classpath");
+            }
+            props.load(input);
+            String user = props.getProperty("db.user");
+            String pass = props.getProperty("db.password");
+            String url = props.getProperty("db.url");
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
             Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
