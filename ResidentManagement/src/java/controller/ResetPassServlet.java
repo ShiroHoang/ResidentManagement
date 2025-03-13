926637153c0caf9c2b95c54e3815d1825860fb06
huynq <huynguyensteph@gmail.com>
2025-03-13 12:44:36 +0700

    update(ResetPass)

--- ResidentManagement/src/java/controller/ResetPassServlet.java
+++ ResidentManagement/src/java/controller/ResetPassServlet.java
@@ -2,14 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
 import dal.UserDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -17,38 +15,40 @@ import model.User;
 
 /**
  *
- * @author AN515-57
+ * @author huyng
  */
-@WebServlet(name="ResetPassServlet", urlPatterns={"/ResetPass"})
 public class ResetPassServlet extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet ResetPassServlet</title>");  
+            out.println("<title>Servlet ResetPassServlet</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet ResetPassServlet at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet ResetPassServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -56,12 +56,13 @@ public class ResetPassServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -69,36 +70,37 @@ public class ResetPassServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-//        processRequest(request, response);
+            throws ServletException, IOException {
         String fullName = request.getParameter("name");
         String email = request.getParameter("email");
         String phoneNum = request.getParameter("phone");
         String newPass = request.getParameter("newPass").trim();
         String confirmPass = request.getParameter("confirmPass").trim();
-        
+
         if (!newPass.equals(confirmPass)) {
             request.setAttribute("error", "Mật khẩu không khớp!");
             request.getRequestDispatcher("view/passwordReset.jsp").forward(request, response);
             return;
         }
-        
+
         UserDAO userdb = new UserDAO();
         User user = userdb.getUserByNamePhoneAndEmail(fullName, email, phoneNum);
-        
+
         if (user == null) {
             request.setAttribute("error", "Thông tin không khớp hoặc không tồn tại!");
             request.getRequestDispatcher("view/passwordReset.jsp").forward(request, response);
             return;
         }
-        
+
         userdb.updateAccountPassword(user.getUserId(), newPass);
         request.setAttribute("success", "Đổi mật khẩu thành công!");
         request.getRequestDispatcher("view/passwordReset.jsp").forward(request, response);
+
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
